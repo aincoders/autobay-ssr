e590f73767459c139d1a9f62846f0a5097feabcd
aincoders <aincoders@gmail.com>
2023-12-21 18:27:44 +0530

    chex

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,18 @@ import { useEffect, useState } from 'react';
 import LoadingScreen from 'src/components/loading-screen/LoadingScreen';
 import { CountryWithCityModal } from 'src/master';
 
-export default function HomePage({ currentCity }) {
+export default function HomePage() {
     const router = useRouter();
 
-    useEffect(() => {
-        if (!currentCity) {
-            setCityModal(true);
-        } else {
-            router.push(currentCity.slug);
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (!currentCity) {
+    //         setCityModal(true);
+    //     } else {
+    //         router.push(currentCity.slug);
+    //     }
+    // }, []);
 
-    const [cityModal, setCityModal] = useState(false);
+    const [cityModal, setCityModal] = useState(true);
     function cityModalClose() {
     }
 
@@ -30,9 +30,9 @@ export default function HomePage({ currentCity }) {
 }
 
 
-export async function getServerSideProps(context) {
-    const { req } = context;
-    const currentCity = req.cookies?.currentCity_v1 ? JSON.parse(req.cookies?.currentCity_v1) : "";
-    const currentVehicle = req.cookies?.currentVehicle ? JSON.parse(req.cookies?.currentVehicle) : { make: '', model: '' };
-    return { props: { currentCity, currentVehicle } };
-}
+// export async function getServerSideProps(context) {
+//     const { req } = context;
+//     const currentCity = req.cookies?.currentCity_v1 ? JSON.parse(req.cookies?.currentCity_v1) : "";
+//     const currentVehicle = req.cookies?.currentVehicle ? JSON.parse(req.cookies?.currentVehicle) : { make: '', model: '' };
+//     return { props: { currentCity, currentVehicle } };
+// }
